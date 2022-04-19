616cd6027c72e6ace2a3760bb6f1f3693a6a0850
shajon ahmed <andshajon@gamil.com>
2022-04-19 04:56:19 +0100

    home page almost done

--- src/components/FooterNavigation/Footer.js
+++ src/components/FooterNavigation/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
     <div className='footer__container'>
     <div className="footer__left">
     <img src="https://lh3.googleusercontent.com/U3PDMXXKOV4UX1TjgTtF43iLVruhYt8hyyzwWqZDi_x8-rXPuXL8p7doAGvTfuTFoB54Qf3c4jYDIoEAFlBn1ekQ8yJm9zS5y7Odbj6hoSK8LVN488bUg_BfdJQyhKjlSsGN6l2p_WdEdmCrdEonEvwvUnrLlfSvutnDjR2pkfEKmaxDpdKINEqa9RiwR6TNyddvbjHOVRS4idK1EpV__CibJl8-g9Y8_yidZ081pB3ORAdSdicK2Adf36S4LoIWoNT6irGPV5GEml3JE6GnHeF8HOEcxMClt89D0r9ESlOJnPXq9pyZBBfhP4Bjc-OKmeaCs8susTAMRCA9rGiMnoVWJZS-H_BjCVxu-CsnegvTpaFaafWXwn_tpY-1vZRqUq24FV1ToevtVgJQRAFlIg7Lo0R7_WMWo-taRlzr58-BOf5v2Ctw2vuIPvq-VQe1j3IlIV4vzSVFAqONuopOULLpcFj0gWwCW2-aVOfNaldrOLh9tvPoNwWprAuHXU1bEAUNq5hHG3VwZNEsjrddLmdslCWRp8WLyAbixJ-iYgg72vju3dUAle4ejji64M-jGLbitQNgSyEt4431koG4x79IvypBeGJ3LUQhh0jzOA1JXJEiudubMjISbs3vmf_WmrRaqtVWwvx_3CkFi0rp4CosGXFRlpFIZqwSEa3P8I8Ltu-D4tRCQjNBZkAoFmSwm1viI8WKe4WJwutRqOTeKE5mi7rHtNVStelp77IxKmdjq7VEmjgr0FyuLMZC_w=s200-no?authuser=0" className="footer__logo"/>
-    <CallToAction backgroundColor="transparent"  color="#DFC0CB"  text="Donate Here"/>
+    <CallToAction backgroundColor="transparent"  color="black"  text="Donate Here"/>
         
  
     
--- src/components/SupportSinglePerson/SupportCard.js
+++ src/components/SupportSinglePerson/SupportCard.js
@@ -4,13 +4,13 @@ import './SupportCard.css';
 
 
 
-function SupportCard({ image ,text, subText, CallToAction}) {
+function SupportCard({ image , title , text , CallToAction}) {
   return (
     <div className='supportCard'>
-       <img src={image} className="supportCard__image"  />
+       <img src={image} alt={title} className="supportCard__image"  />
        <div className='supportCard__textContainer'>
            <h2>{text}</h2>
-           <p className='supportCard__subText'>{subText}</p>
+          
            <div className='supportCard__callToAction'>{CallToAction}</div>
 
        </div>
